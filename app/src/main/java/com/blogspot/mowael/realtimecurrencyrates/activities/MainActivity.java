b0f9e3ed97f4efe662d7c6b785efa4ab6b0e90ec
Mohamed Wael <Mohamed.wael9371@gmail.com>
2016-12-03 21:12:26 +0200

    performance modification layout modification

--- app/src/main/java/com/blogspot/mowael/realtimecurrencyrates/activities/MainActivity.java
+++ app/src/main/java/com/blogspot/mowael/realtimecurrencyrates/activities/MainActivity.java
@@ -15,7 +15,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.blogspot.mowael.realtimecurrencyrates.R;
 import com.blogspot.mowael.realtimecurrencyrates.adapters.DialogAdapter;
 import com.blogspot.mowael.realtimecurrencyrates.adapters.DotLoadlerDialogAdapter;
